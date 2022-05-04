4c49a9cffb4030fb04c015accf2474e7dcd909cd
Okellothomas <thomasolal33@gmail.com>
2022-05-04 19:15:25 +0300

    Add variables to the squad object

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,5 +1,37 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Squad {
+    // declaring the variables for Squad
+    private String sSize;
+    private String sName;
+    private String sCause;
+    private List<Hero> sHeros;
+    private static List<Squad> sInstances = new ArrayList<>();
+    private int sId;
+
+    public Squad(String sSize, String sName, String sCause) {
+        this.sSize = sSize;
+        this.sName = sName;
+        this.sCause = sCause;
+        sInstances.add(this);
+        this.sId = sInstances.size();
+        this.sHeros = new ArrayList<Hero>();
+    }
+
+    public String getsSize() {
+        return sSize;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public String getsCause() {
+        return sCause;
+    }
+
 
 }
