d6991c88c40978ece7e88c61cef2e185ac07fe19
Okellothomas <thomasolal33@gmail.com>
2022-05-04 15:23:31 +0300

    Add the hero privet members and the constructor

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,5 +1,60 @@
 package models;
 
-public class Hero {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Hero {
+    private String mName;
+    private int mAge;
+    private String mSpecialPower;
+    private String mWeaknesses;
+    private boolean created;
+    private static List<Hero> mHeroes = new ArrayList<>();
+    private int mId;
+
+    public Hero(String mName, int mAge, String mSpecialPower, String mWeaknesses) {
+        this.mName = mName;
+        this.mAge = mAge;
+        this.created = false;
+        this.mSpecialPower = mSpecialPower;
+        this.mWeaknesses = mWeaknesses;
+        mHeroes.add(this);
+        this.mId = mHeroes.size();
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public int getmAge() {
+        return mAge;
+    }
+
+    public String getmSpecialPower() {
+        return mSpecialPower;
+    }
+
+    public String getmWeaknesses() {
+        return mWeaknesses;
+    }
+
+    public static List<Hero> getmHeroes() {
+        return mHeroes;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public static void clearAllHeros(){
+        mHeroes.clear();
+    }
+
+    public boolean getPublished(){
+        return this.created;
+    }
+
+    public static Hero findById(int mId){
+        return mHeroes.get(mId - 1);
+    }
 }
