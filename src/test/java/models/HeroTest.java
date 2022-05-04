76f2d5dbe4dbba15ed2514a361c5808ea3508e27
Okellothomas <thomasolal33@gmail.com>
2022-05-04 15:24:26 +0300

    test if a hero has been created

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,7 +1,13 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HeroTest {
-
+    @Test
+    public void newHeroObjectGetCreated_true() throws Exception{
+        Hero hero = new Hero("Messi", 46, "Tribbler","eating");
+        assertEquals(true, hero instanceof Hero);
+    }
 }
