3ccb669b46850a8b34258d6807f28bfcb789fbe8
Okellothomas <thomasolal33@gmail.com>
2022-05-04 15:25:42 +0300

    check whether instance of the hero has been created

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -10,4 +10,43 @@ public class HeroTest {
         Hero hero = new Hero("Messi", 46, "Tribbler","eating");
         assertEquals(true, hero instanceof Hero);
     }
+    @Test
+    public void getmName_myHeoresName_String(){
+        Hero hero = new Hero("Messi", 46, "Tribbler","eating");
+        String mName = "Messi";
+        assertEquals("Messi", hero.getmName());
+    }
+
+    @Test
+    public void getmAge_myHeoresAge_Int(){
+        Hero hero = new Hero("Messi", 46, "Tribbler","eating");
+        int mAge = 46;
+        assertEquals(46, hero.getmAge());
+    }
+    @Test
+    public void getmSpecialPower_myHeoresName_String(){
+        Hero hero = new Hero("Messi", 46, "Tribbler","eating");
+        String mSpecialpower = "Tribbler";
+        assertEquals(mSpecialpower, hero.getmSpecialPower());
+    }
+
+    @Test
+    public void getmWeaknesses_myHeoresWeaknesses_String(){
+        Hero hero = new Hero("Messi", 46, "Tribbler","eating");
+        String mWeaknesses = "eating";
+        assertEquals(mWeaknesses, hero.getmWeaknesses());
+    }
+
+    @Test
+    public void getPublished_createdHeroPublished_false(){
+        Hero hero = new Hero("Messi", 46, "Tribbler","eating");
+        assertEquals(false, hero.getPublished());
+    }
+
+    @Test
+    public void getId_HeroInstantiatedWithAnId_1(){
+        Hero.clearAllHeros();
+        Hero hero = new Hero("Messi", 46, "Tribbler","eating");
+        assertEquals(1,hero.getmId());
+    }
 }
