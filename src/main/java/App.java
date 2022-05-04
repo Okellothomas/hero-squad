4dfa0204a98338c3f896cd8fdfde6b130be14430
Okellothomas <thomasolal33@gmail.com>
2022-05-04 15:33:06 +0300

    Add the main route to the project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,18 @@
+import java.util.List;
+import java.util.Map;
 import java.util.HashMap;
+import models.*;
 import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-
+        post("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
