0cc54d0e719c9fbf46a6253741bec244bf77c024
Okellothomas <thomasolal33@gmail.com>
2022-05-04 18:30:54 +0300

    Add myheros.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import models.*;
 import spark.ModelAndView;
 import static spark.Spark.*;
@@ -13,6 +15,12 @@ public class App {
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        // the route for myheros:
+        post("/myheros", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "myheros.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
